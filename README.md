a0ea2b575100f817897484b5288fcaac8d1cd506
Kevin Albertson <kevin.albertson@mongodb.com>
2020-04-17 14:42:46 -0400

    updates for CDRIVER-3614

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 To run the manual tests for SRV polling there's a bit of setup. You need:
 1. a DNS server
-2. a sharded cluster with multiple mongoses running with SSL
+2. a sharded cluster with multiple mongoses running with TLS
 3. some way to observe the scanning behavior
 
 ## Step 1: A local DNS server ##
@@ -30,21 +30,25 @@ Next, to test this locally, set your default DNS to point to this local server. 
 Be careful though. This overrides the default DNS lookup and renders you offline. Remove this entry after testing.
 
 ## Step 2: A sharded cluster ##
-You'll need four mongoses for the tests, and SSL configured. The client/server pem files are included in this repo.
+You'll need four mongoses for the tests, and TLS configured. The client/server pem files are included in this repo.
 
 ```
-mlaunch init --replicaset --nodes=1 --sharded=1 --mongos 3 --hostname localhost --enableMajorityReadConcern --setParameter enableTestCommands=1 --setParameter="logComponentVerbosity={command:3}" --sslMode=allowSSL --sslPEMKeyFile=server.pem --sslCAFile=ca.pem --sslClientPEMKeyFile=./client.pem --sslClientCertificate=client.pem --sslAllowInvalidCertificates  --dir ./srv-sharded
+mlaunch init --replicaset --nodes=1 --sharded=1 --mongos 4 --hostname localhost --setParameter enableTestCommands=1 --setParameter="logComponentVerbosity={command:3}" --tlsMode=allowTLS --tlsCertificateKeyFile=server.pem --tlsCAFile=ca.pem --tlsAllowConnectionsWithoutCertificates --dir ./srv-sharded
 ```
 
 ## Step 3: Running tests and observing ##
 The simplest (though hacky) way is to modify the C driver and example-client.
 - Set MONGOC_TOPOLOGY_MIN_RESCAN_SRV_INTERVAL_MS to 1000 in mongoc-topology-private.h (since the DNS records have TTL of 1 second, this means a scan will occur every second, instead of every 60 seconds).
 - Copy example-client-modified.c from this repo over example-client.c. This modified example-client prints the host list every second after running a find command for 60 seconds total.
-- Run example-client, passing a URI with SSL options and a reduced heartbeatFrequencyMS:
+- Run example-client
+    - The first argument is the URI. Passing a URI with TLS options (just use tlsinsecure since TLS isn't what we're testing). Set heartbeatFrequencyMS to 1000 so scanning happens every second. And if the initial SRV lookup resolves to one host the topology description will enter "Single" and polling will be bypassed. Pass `directConnection=false` to prevent this.
+    - The second argument is either SINGLE or POOLED to test single or pooled client monitoring. Test each scenario.
 ```
-./cmake-build/src/libmongoc/example-client "mongodb+srv://server.mongodb.com/?tls=true&tlscertificatekeyfile=client.pem&tlsallowinvalidcertificates=true&heartbeatFrequencyMS=500"
+./cmake-build/src/libmongoc/example-client "mongodb+srv://server.mongodb.com/?tls=true&tlsinsecure=true&heartbeatFrequencyMS=1000&directConnection=false" SINGLE
 ```
+
 If all goes well, you'll see a printout of all known hosts every second. This will happen for 60 seconds. You can kill the DNS server and restart it with different records to test the different scenarios.
+
 - As a bonus, build example-client with the C flag `-fsanitize=address` and set the following environment variables:
     - `export ASAN_OPTIONS='detect_leaks=1'`
     - `export LSAN_OPTIONS='suppressions=.lsan-suppressions'`
--- example-client-modified.c
+++ example-client-modified.c
@@ -1,12 +1,14 @@
 /*
 Manually modify MONGOC_TOPOLOGY_MIN_RESCAN_SRV_INTERVAL_MS to be 1 second.
 And ensure the TTLs are also 1 second.
+Set heartbeatFrequencyMS in the URI to 1 second.
  */
 
 #include <mongoc/mongoc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define MONGOC_INSIDE
 #include "mongoc/mongoc-client-private.h"
 #include "mongoc/mongoc-topology-private.h"
 #include "mongoc/mongoc-uri-private.h"
@@ -30,91 +32,136 @@ struct _mongoc_uri_t {
 };
 
 void
-_mongoc_host_list_dump (const mongoc_host_list_t *host) {
-   printf("host_list:\n");
+_host_list_dump (const mongoc_host_list_t *host)
+{
+   MONGOC_DEBUG ("host_list:");
    while (host != NULL) {
-      printf("-%s\n", host->host_and_port);
+      printf ("- %s\n", host->host_and_port);
       host = host->next;
+   }
+}
+
+
+static const char *
+_sd_type (mongoc_server_description_t *sd)
+{
+   switch (sd->type) {
+   case MONGOC_SERVER_RS_PRIMARY:
+      return "MONGOC_SERVER_RS_PRIMARY";
+   case MONGOC_SERVER_RS_SECONDARY:
+      return "MONGOC_SERVER_RS_SECONDARY";
+   case MONGOC_SERVER_STANDALONE:
+      return "MONGOC_SERVER_STANDALONE";
+   case MONGOC_SERVER_MONGOS:
+      return "MONGOC_SERVER_MONGOS";
+   case MONGOC_SERVER_POSSIBLE_PRIMARY:
+      return "MONGOC_SERVER_POSSIBLE_PRIMARY";
+   case MONGOC_SERVER_RS_ARBITER:
+      return "MONGOC_SERVER_RS_ARBITER";
+   case MONGOC_SERVER_RS_OTHER:
+      return "MONGOC_SERVER_RS_OTHER";
+   case MONGOC_SERVER_RS_GHOST:
+      return "MONGOC_SERVER_RS_GHOST";
+   case MONGOC_SERVER_UNKNOWN:
+      return "MONGOC_SERVER_UNKNOWN";
+   case MONGOC_SERVER_DESCRIPTION_TYPES:
+   default:
+      return "??";
+   }
+   return "??";
+}
+
+static void
+_topology_description_dump (mongoc_topology_description_t *td)
+{
+   int i;
+   mongoc_set_t *set;
+   mongoc_server_description_t *sd;
+
+   set = td->servers;
+
+   MONGOC_DEBUG ("Topology description dump (%s):",
+                 mongoc_topology_description_type (td));
+
+   for (i = 0; i < set->items_len; ++i) {
+      mongoc_host_list_t *host;
+      sd = (mongoc_server_description_t *) mongoc_set_get_item (set, (int) i);
+
+      host = mongoc_server_description_host (sd);
+      printf ("- %s, %s\n", host->host_and_port, _sd_type (sd));
    }
 }
 
 int
 main (int argc, char *argv[])
 {
+   mongoc_client_pool_t *pool;
    mongoc_client_t *client;
-   mongoc_collection_t *collection;
-   mongoc_cursor_t *cursor;
    bson_error_t error;
-   const bson_t *doc;
-   const char *collection_name = "test";
-   bson_t query;
+   bson_t *cmd;
    char *str;
-   const char *uri_string = "mongodb://127.0.0.1/?appname=client-example";
+   char *uri_string;
    mongoc_uri_t *uri;
+   bool pooled;
 
    mongoc_init ();
-   if (argc > 1) {
-      uri_string = argv[1];
+   if (argc != 3) {
+      MONGOC_ERROR ("Usage: ./example-client <URI> <\"POOLED\"|\"SINGLE\">\n");
+      return EXIT_FAILURE;
    }
 
-   if (argc > 2) {
-      collection_name = argv[2];
-   }
+   uri_string = argv[1];
+   pooled = (strcasecmp (argv[2], "POOLED") == 0);
 
    uri = mongoc_uri_new_with_error (uri_string, &error);
    if (!uri) {
-      fprintf (stderr,
-               "failed to parse URI: %s\n"
-               "error message:       %s\n",
-               uri_string,
-               error.message);
+      MONGOC_ERROR ("failed to parse URI: %s, error message: %s\n",
+                    uri_string,
+                    error.message);
       return EXIT_FAILURE;
    }
 
-   client = mongoc_client_new_from_uri (uri);
-   if (!client) {
-      return EXIT_FAILURE;
+   if (pooled) {
+      pool = mongoc_client_pool_new (uri);
+      BSON_ASSERT (pool);
+      mongoc_client_pool_set_error_api (pool, 2);
+      client = mongoc_client_pool_pop (pool);
+   } else {
+      client = mongoc_client_new_from_uri (uri);
+      BSON_ASSERT (client);
+      mongoc_client_set_error_api (client, 2);
    }
-
-   mongoc_client_set_error_api (client, 2);
-
-   bson_init (&query);
-
-#if 0
-   bson_append_utf8 (&query, "hello", -1, "world", -1);
-#endif
 
    int secs = 0;
+   cmd = BCON_NEW ("ping", BCON_INT32 (1));
    while (true) {
-      collection = mongoc_client_get_collection (client, "test", collection_name);
-      cursor = mongoc_collection_find_with_opts (
-         collection,
-         &query,
-         NULL,  /* additional options */
-         NULL); /* read prefs, NULL for default */
+      bson_t reply;
+      bool ret;
 
-      while (mongoc_cursor_next (cursor, &doc)) {
-         str = bson_as_canonical_extended_json (doc, NULL);
-         fprintf (stdout, "%s\n", str);
-         bson_free (str);
-      }
+      ret = mongoc_client_command_simple (
+         client, "admin", cmd, NULL, &reply, &error);
+      str = bson_as_json (&reply, NULL);
+      MONGOC_DEBUG ("reply = %s", str);
+      bson_free (str);
+      bson_destroy (&reply);
 
-      if (mongoc_cursor_error (cursor, &error)) {
-         fprintf (stderr, "Cursor Failure: %s\n", error.message);
+      if (!ret) {
+         MONGOC_ERROR ("ping failure: %s\n", error.message);
          return EXIT_FAILURE;
       }
-      printf("it has been %d seconds\n", secs);
+      printf ("it has been %d seconds\n\n", secs);
       secs++;
-      sleep(1);
+      sleep (1);
 
-      _mongoc_host_list_dump (client->topology->uri->hosts);
+      bson_mutex_lock (&client->topology->mutex);
+      _host_list_dump (client->topology->uri->hosts);
+      _topology_description_dump (&client->topology->description);
+      bson_mutex_unlock (&client->topology->mutex);
 
-      mongoc_cursor_destroy (cursor);
-      mongoc_collection_destroy (collection);
-      if (secs > 60) break;
+      if (secs > 60)
+         break;
    }
 
-   bson_destroy (&query);
    mongoc_uri_destroy (uri);
    mongoc_client_destroy (client);
    mongoc_cleanup ();
